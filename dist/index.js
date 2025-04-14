65a740653bd147e174c61491f1d3c6a794110261
grimlink <sean.grimlink@gmail.com>
2025-04-15 00:23:25 +0200

    UPD: Build

--- dist/index.js
+++ dist/index.js
@@ -6,11 +6,12 @@
       const evaluate = expression.length ? evaluateLater(expression) : () => {
       };
       const lockPageScroll = modifiers.includes("noscroll");
+      const closeby = modifierValue(modifiers, "closeby", "any");
       el.style.display = null;
       el.hidden = false;
       el.style.length === 0 && el.removeAttribute("style");
       el.addEventListener("keydown", escapeDialog);
-      el.addEventListener("mousedown", backdropDialog);
+      el.addEventListener("click", backdropDialog);
       el.addEventListener("submit", methodDialog);
       function scrollLock(use = true) {
         document.body.style.overflow = use ? "hidden" : "";
@@ -21,33 +22,59 @@
         }
       }
       function escapeDialog(event) {
-        if (event.key !== "Escape") return;
+        if (event.key !== "Escape")
+          return;
         event.preventDefault();
+        if (closeby !== "any" && closeby !== "closerequest")
+          return;
         evaluate();
       }
       function backdropDialog(event) {
+        if (event.target !== el || closeby !== "any")
+          return;
         const rect = el.getBoundingClientRect();
         const isInDialog = rect.top <= event.clientY && event.clientY <= rect.top + rect.height && rect.left <= event.clientX && event.clientX <= rect.left + rect.width;
-        if (isInDialog) return;
+        if (isInDialog)
+          return;
         evaluate();
       }
       el._x_doShow = () => {
-        if (el.hasAttribute("open")) return;
+        if (el.hasAttribute("open"))
+          return;
         el.showModal();
         scrollLock(lockPageScroll);
       };
       el._x_doHide = () => {
-        if (!el.hasAttribute("open")) return;
+        if (!el.hasAttribute("open"))
+          return;
         el.close();
         scrollLock(false);
       };
       cleanup(() => {
         el.removeEventListener("keydown", escapeDialog);
-        el.removeEventListener("mousedown", backdropDialog);
+        el.removeEventListener("click", backdropDialog);
         el.removeEventListener("submit", methodDialog);
         scrollLock(false);
       });
     }
+  }
+  function modifierValue(modifiers, key, fallback) {
+    if (modifiers.indexOf(key) === -1)
+      return fallback;
+    const rawValue = modifiers[modifiers.indexOf(key) + 1];
+    if (!rawValue)
+      return fallback;
+    if (key === "closeby") {
+      const allowedValues = ["auto", "none", "closerequest", "any"];
+      const options = allowedValues.join(", ");
+      if (!allowedValues.includes(rawValue)) {
+        console.warn(
+          `"${rawValue}" is not one of the allowed values for closeby: ${options}`
+        );
+      }
+      return fallback;
+    }
+    return rawValue;
   }
 
   // src/cdn.js
